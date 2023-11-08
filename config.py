0a801b23aaa4c54b1e54b87e6da73c097a45a035
njeikarp <njeikarp@cisco.com>
2023-11-08 18:35:58 +0530

    DB config

--- config.py
+++ config.py
@@ -1,14 +1,8 @@
-import configparser
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
-config = configparser.ConfigParser()
-config.read('config.ini')
-
-Elastic_Username = config['ELASTIC']['user']
-Elastic_Password = config['ELASTIC']['password']
-
-Sync_Path = config['SYNC']['base_path']
-
-Security_Key = config['SECURITY']['secret']
-Security_Algo = config['SECURITY']['algorithm']
+Elastic_Username = 'elastic'
+Elastic_Password = os.getenv("ELASTIC_PASSWORD", "qwerty")
 
 FTP_BASE_PATH = "."
